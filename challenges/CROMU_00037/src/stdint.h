ec219fd909928baf5329204d9529c70670d6ee77
novafacing <rowanbhart@gmail.com>
2024-11-24 22:13:16 -0800

    Some updates

---
+++ challenges/CROMU_00037/src/stdint.h
@@ -0,0 +1,54 @@
+/*
+
+Author: Jason Williams <jdw@cromulence.com>
+
+Copyright (c) 2014 Cromulence LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+#ifndef __STDINT_H__
+#define __STDINT_H__
+
+typedef signed char int8_t;
+typedef short int int16_t;
+typedef int int32_t;
+typedef long long int int64_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long int uint64_t;
+
+/* Types for `void *' pointers.  */
+#if defined(_WIN64) || defined(__LP64__) || defined(_LP64)
+#ifndef __intptr_t_defined
+typedef long int intptr_t;
+#define __intptr_t_defined
+#endif
+typedef unsigned long int uintptr_t;
+#else
+#ifndef __intptr_t_defined
+typedef int intptr_t;
+#define __intptr_t_defined
+#endif
+typedef unsigned int uintptr_t;
+#endif
+
+#endif  // __STDINT_H__
